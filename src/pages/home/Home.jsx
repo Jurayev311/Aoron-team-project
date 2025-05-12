ac4743d77295fce7983193371a98cdcfa806a842
Zafarbek Qurbonboyev <dasturchiqurbonboyev@gmail.com>
2025-05-12 14:48:05 +0500

    added hero component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../../components/homeComponent/hero/Hero'
+import HomeFeatureProduct from '../../components/homeComponent/homeFeatureProduct/HomeFeatureProduct'
 
 const Home = () => {
   return (
-    <div className='pt-[100px]'>Home</div>
+    <div className='pt-[75px]'>
+      <Hero />
+      <HomeFeatureProduct />
+    </div>
   )
 }
 
