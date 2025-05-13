cc3ecfe0c7a165b65b79724d7b421c55c1edf40e
Zafarbek Qurbonboyev <dasturchiqurbonboyev@gmail.com>
2025-05-13 14:22:50 +0500

    added productss

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Hero from '../../components/homeComponent/hero/Hero'
+import HomeFeatureProduct from '../../components/homeComponent/homeFeatureProduct/HomeFeatureProduct'
+import HeroArrivals from '../../components/homeComponent/heroArrivals/HeroArrivals'
 
 const Home = () => {
   return (
     <div className='pt-[75px] '>
       <Hero/>
+      <HomeFeatureProduct/>
+      <HeroArrivals/>
     </div>
+
   )
 }
 
