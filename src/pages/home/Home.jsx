5db9ecbc7e417936cf6cc2b1804536f42ee657d3
Zafarbek Qurbonboyev <dasturchiqurbonboyev@gmail.com>
2025-05-13 13:17:19 +0500

    adderd hero components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../../components/homeComponent/hero/Hero'
 
 const Home = () => {
   return (
-    <div className='pt-[75px] '>Home</div>
+    <div className='pt-[75px] '>
+      <Hero/>
+    </div>
   )
 }
 
